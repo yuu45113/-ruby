bfa564e4186f45730377a3b762dc3f882a4e26db
EC2 Default User <ec2-user@ip-172-31-9-71.ap-northeast-1.compute.internal>
2023-04-12 13:06:44 +0000

    Initial Commit

---
+++ class.rb
@@ -0,0 +1,8 @@
+array=[1,2,3]
+puts array.class
+puts array.join("*")
+
+string="Hello"
+puts string.class
+puts string.join("*")
+
---
+++ new.rb
@@ -0,0 +1,25 @@
+#インスタンスメソッド
+class Car
+  
+  def turn(direction)
+    puts"#{direction}に曲がります。"
+  end
+  
+  def run(distance)
+    puts"車で#{distance}キロ走ります。"
+  end
+end
+car=Car.new
+car.turn("右")
+
+car=Car.new
+car.run(5)
+
+#クラスメソッド
+class Car
+  def self.run(distance)
+    puts"車で#{distance}キロ走ります。"
+  end
+end 
+
+Car.run(10)
